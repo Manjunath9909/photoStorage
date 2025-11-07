4715e34b91f84451c0d21e3af76a6c9c16c4bc0f
/asdasdasd <97905123+ConanTheBarbarianCodes@users.noreply.github.com>
2025-11-07 23:10:14 +0530

    some changes to routes

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import url_for
+from flask import redirect
 
 app = Flask(__name__)
 
-bigDictionaryOfMovies = {'movies':[{"Tramsformers 1", "Transformers 2", "Traosformers 4"}]}
+bigDictionaryOfMovies = {"movies":
+                         [{"id": 1, "name" : "Transformers 1"},
+                         {"id" : 2, "name" : "Transformers 2"},
+                         {"id" : 3, "name" : "Transformers 3"}]
+                         }
 
 @app.route('/')
 def index():
-    #return render_template('index.html')
-    return bigDictionaryOfMovies['movies']
-
+    return render_template('index.html')
+    
 @app.route('/movies')
 def movies():
     return render_template('viewmovies.html')
 
-@app.route('/addmovie')
+@app.route('/addmovie', methods= ['POST','GET'])
 def addmovie():
-    return render_template('addmovie.html')
-
+    if request.method == 'POST':
+        
+        return redirect(url_for('index'))
+    else:
+        return render_template('addmovie.html')
+    
 @app.route('/games')
 def games():
     return render_template('viewmovies.html')
@@ -26,3 +35,10 @@ def games():
 @app.route('/addgame')
 def addgame():
     return render_template('viewmovies.html')
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('movies'))
+    print(url_for('addmovie', movie_name = 'Transform4'))
+    print(url_for('games'))
+    print(url_for('addgame'))
--- tests.py
+++ tests.py
@@ -6,5 +6,11 @@ bigDictionaryOfMovies = {"movies":
                          {"id" : 3, "name" : "Transformers 3"}]
                         }
 
-print(bigDictionaryOfMovies['movies'][0]["name"])
+bigDictionaryOfMovies['movies'].append({"id" : 4, "name" : "Transformers 4"})
+
+print(bigDictionaryOfMovies['movies'].__len__())
+len = bigDictionaryOfMovies['movies'].__len__()
+bigDictionaryOfMovies['movies'].append({"id" : len + 1, "name" : "Transformers 5"})
+
+print(bigDictionaryOfMovies['movies'])
 
